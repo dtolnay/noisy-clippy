0d565b657a88b5ff8fb34fd1f2712a3c9949b08c
David Tolnay <dtolnay@gmail.com>
2025-03-02 20:40:52 -0800

    Ignore elidable_lifetime_names pedantic clippy lint
        warning: the following explicit lifetimes could be elided: 'a
          --> src/main.rs:78:12
           |
        78 | impl<'ast, 'a> Visit<'ast> for AttrVisitor<'a> {
           |            ^^                              ^^
           |
           = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#elidable_lifetime_names
           = note: `-W clippy::elidable-lifetime-names` implied by `-W clippy::pedantic`
           = help: to override `-W clippy::pedantic` add `#[allow(clippy::elidable_lifetime_names)]`
        help: elide the lifetimes
           |
        78 - impl<'ast, 'a> Visit<'ast> for AttrVisitor<'a> {
        78 + impl<'ast> Visit<'ast> for AttrVisitor<'_> {
           |

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
     clippy::cast_possible_wrap,
     clippy::derive_partial_eq_without_eq,
     clippy::doc_markdown,
+    clippy::elidable_lifetime_names,
     clippy::items_after_statements,
     clippy::let_underscore_untyped,
     clippy::match_same_arms,
